82c84cf311cf2e237d99237428e75db8a109d830
Andrew Waller <andrewhwaller@gmail.com>
2019-04-22 02:07:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+    PigLatinizer.new
+    piglatinize(params[:user_input])
+  end
+
+  post '/results'
+    erb :results
 end
